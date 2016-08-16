d27f45b1bc32db07a1b57a3c3d69a59d6a79c457
sunwonl <sun2208@gmail.com>
2016-08-16 21:40:55 +0900

    annotation2

--- postprocess/annotationScore.py
+++ postprocess/annotationScore.py
@@ -3,8 +3,8 @@ Created on 2016. 8. 16.
 
 @author: sun
 '''
-from gprofiler import GProfiler
 import json
+import os
 from postprocess.goEnrichment import GOEnrichmentTester
 
 def jaccard(list1, list2):
@@ -27,3 +27,45 @@ def getscore(termlist, disease, annotation):
     
     return jaccard(annotatedTerms_answer, annotatedTerms_query)
 
+def getAllDescendantFiles(root):
+    retfiles = []
+    
+    for (path, dir, files) in os.walk(root):
+        for filename in files:
+            retfiles.append ( (path,filename) )
+    return retfiles
+
+rootdir = "Q:/COSSY+/misResultCSVOnly/ICGC_TCGA/fortest/"
+misrootdirs = {"BRCA":getAllDescendantFiles(rootdir + "BRCA/"), "COAD":getAllDescendantFiles(rootdir + "COAD/"), "LUSC":getAllDescendantFiles(rootdir + "LUSC"), "PRAD":getAllDescendantFiles(rootdir + "PRAD"), "STAD":getAllDescendantFiles(rootdir + "STAD")}
+
+gotester = GOEnrichmentTester()
+
+cosmicFilename = "G:/cossy++/cosmicGO.json"
+cosmicAnswer = loadCOSMICAnnotation(cosmicFilename)
+
+
+for dataset in misrootdirs:
+    files = misrootdirs[dataset]
+    
+    for (path, filename) in files:
+        print path
+        print filename
+        with open(path + "/" + filename,'r') as r:
+            for line in r:
+                tokens = [x.strip() for x in line.replace('\"','').split(",") if len(x.strip()) > 0]
+                
+                if tokens[0] == 'gene1':
+                    continue;
+                
+                genelist = tokens[1:len(tokens)]
+                
+                goTerms = [x["term"] for x in gotester.getGoTerms(genelist)]
+                cosmicTerms = [x["term"] for x in cosmicAnswer[dataset]]
+                
+                score = jaccard(goTerms, cosmicTerms)
+                
+                result = 
+                print tokens
+        break
+    
+    break
