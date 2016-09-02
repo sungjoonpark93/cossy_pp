56b999e6cdf67f9b7d78b4845ef487e1e1abaaaf
sunwonl <sun2208@gmail.com>
2016-09-02 18:59:25 +0900

    test codes

---
+++ icossy_experiment/icossy_experiment.py
@@ -0,0 +1,33 @@
+'''
+Created on 2016. 9. 2.
+
+@author: sun
+'''
+from icossy import parameter
+from icossy.centralEngine import cossyPlus
+
+if __name__ == '__main__':
+    
+    datasets = ["BRCA", "COAD", "LUSC", "PRAD", "STAD"]
+    
+    analysis_types = ["expression", "mut_with_exp", "mutation"]
+    misFiles = {"kegg":"kegg_cossy_symbol.gmt", "keggClusterOne":"keggWhole_clusterONE_symbol.gmt", "stringClusterOne":"string_clusterONE_symbol.gmt", "string":"string_cossy_symbol.gmt"}
+    
+    for dataset in datasets:
+        for aType in analysis_types:
+            for mistype in misFiles:
+                misFile = misFiles[mistype]
+                exp_data_dir = "Q:/COSSY+/data/preprocessed/TCGA/ICGC/release21/" + dataset + "/for_test/"    
+                misoutput_dir = "Q:/COSSY+/mis_result/iCOSSY/TCGA_ICGC/"
+            
+                exp_p = parameter.Parameter()
+    #            exp_p.analyze_type = "expression"
+                exp_p.analyze_type = aType
+                exp_p.exp_file = exp_data_dir + "exp."+dataset+"-US.tsv_preprocessed.gct"
+                exp_p.mutation_file = exp_data_dir + "mut."+dataset+"-US.tsv_preprocessed.csv"
+                exp_p.misResult_file = misoutput_dir + dataset+"/"+aType+"_icossy_"+dataset+"_"+mistype+"_result.txt"
+                # exp_p.gmt_file = "Q:/COSSY+/data/mis/icossy/kegg_cossy_symbol.gmt"
+                exp_p.gmt_file = "Q:/COSSY+/data/mis/icossy/"+misFile
+                
+                cossyPlus(exp_p)
+    
