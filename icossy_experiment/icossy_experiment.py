c7c3ce54b682dfbea45fca34b3e266da393ae7ea
sunwonl <sun2208@gmail.com>
2016-09-06 12:42:01 +0900

    test start

--- icossy_experiment/icossy_experiment.py
+++ icossy_experiment/icossy_experiment.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     
     datasets = ["BRCA", "COAD", "LUSC", "PRAD", "STAD"]
     
-    analysis_types = ["expression", "mut_with_exp", "mutation"]
+    #analysis_types = ["expression", "mut_with_exp", "mutation"]
+    analysis_types = ["expression"]
     misFiles = {"kegg":"kegg_cossy_symbol.gmt", "keggClusterOne":"keggWhole_clusterONE_symbol.gmt", "stringClusterOne":"string_clusterONE_symbol.gmt", "string":"string_cossy_symbol.gmt"}
     
     for dataset in datasets:
@@ -27,7 +28,9 @@ if __name__ == '__main__':
                 exp_p.mutation_file = exp_data_dir + "mut."+dataset+"-US.tsv_preprocessed.csv"
                 exp_p.misResult_file = misoutput_dir + dataset+"/"+aType+"_icossy_"+dataset+"_"+mistype+"_result.txt"
                 # exp_p.gmt_file = "Q:/COSSY+/data/mis/icossy/kegg_cossy_symbol.gmt"
+#                exp_p.smoothing_source_file = ""
                 exp_p.gmt_file = "Q:/COSSY+/data/mis/icossy/"+misFile
                 
                 cossyPlus(exp_p)
     
+                print "done! : " + dataset + " / " + aType + " / " + mistype
